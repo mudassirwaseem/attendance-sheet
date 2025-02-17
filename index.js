6e729fd1917d65ba10e53d00afec7f247c8a5855
mudassirwaseem <mudassir@geeksofkolachi.com>
2025-02-17 20:59:22 +0500

    feat: add monthly HR report and weekly device logs cleanup cron jobs

--- index.js
+++ index.js
@@ -2,18 +2,34 @@
 require('dotenv').config();
 const cron = require('node-cron');
 const connectDB = require('./db');
-const { fetchAndStoreAttendance } = require('./services/attendanceService');
+const { fetchAndStoreAttendance, sendMonthlyReportToHR , removeDeviceLogs } = require('./services/attendanceService');
 
 async function startApp() {
     await connectDB(); 
 
+// **Daily Cron Job (Runs at 5 AM PST)**
     cron.schedule('0 5 * * *', async () => {
-        console.log('⏳ Running attendance sync (every minute)...');
         await fetchAndStoreAttendance();
     });
 
-    console.log("🚀 Attendance cron job scheduled (Runs every minute)");
-    await fetchAndStoreAttendance(); // Run immediately on startup
+
+// **Monthly Cron Job (Runs on the 1st of Every Month at 6 AM PST)**
+    cron.schedule('0 6 1 * *', async () => {
+        console.log('📤 Running monthly HR report job at 6 AM PST...');
+        await sendMonthlyReportToHR();
+        console.log('✅ Monthly HR report sent.');
+    }, { timezone: "Asia/Karachi" });
+    
+
+    cron.schedule('0 6 * * 0', async () => {
+        console.log('📤 Running weekly device logs cleanup job at 6 AM PST...');
+        await removeDeviceLogs();
+        console.log('✅ Device logs removed.');
+    }, { timezone: "Asia/Karachi" });
+
+    // console.log("🚀 Attendance cron job scheduled (Runs every minute)");
+    // await fetchAndStoreAttendance();
+    // await sendMonthlyReportToHR();
 }
 
 startApp();
